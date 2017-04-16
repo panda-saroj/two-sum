32732611f8bffbbc36712b2b6fc99ad93ff2c795
Saroj Panda <saroj.panda@mavs.uta.edu>
2017-04-16 17:39:06 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+    vector<int> result;
+	vector<int> copy;
+    copy = nums;
+	sort(copy.begin(),copy.end());
+        for(int i = 0,j = nums.size()-1;;){
+		  if(copy[i] + copy[j] < target)
+		    i++;
+		  else if(copy[i] + copy[j] > target)
+		    j--;
+		  else{
+		    int index1 = find(nums.begin(),nums.end(),copy[i]) - nums.begin();
+			int index2;
+			if(copy[i] == copy[j]){
+			  index2 = find(nums.begin()+index1 + 1 ,nums.end(),copy[j]) - nums.begin();
+//            index2 = find(nums.begin(),nums.end(),copy[j]) - nums.begin();
+			}
+			else
+			  index2 = find(nums.begin(),nums.end(),copy[j]) - nums.begin();
+            result.push_back(index1);
+            result.push_back(index2);
+
+
+  			return result;
+		  }
+	    }
+    }
+
+};
+
+int main()
+{
+
+    vector<int> v = {3,2,3};
+    int target = 6;
+
+    Solution sol;
+    vector<int> res = sol.twoSum(v, target);
+
+
+    for(int i=0; i< res.size(); i++)
+        cout<<res[i]<<" "<<endl;
+
+
+}
